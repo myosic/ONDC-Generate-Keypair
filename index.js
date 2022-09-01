d92e7831cc1b88e2cb89e787f69a6da75450801e
Tanmoy Sarkar <ts741127@gmail.com>
2022-09-01 18:19:58 +0530

    added codes

---
+++ index.js
@@ -0,0 +1,30 @@
+const _sodium = require('libsodium-wrappers');
+const fs = require('fs');
+
+(async()=>{
+    await _sodium.ready;
+    const sodium = _sodium;
+    // Generate signing key pair
+    const signing_key = sodium.crypto_sign_keypair();
+    const signing_key_public_base64 = sodium.to_base64(signing_key.publicKey, _sodium.base64_variants.ORIGINAL);
+    const signing_key_private_base64 = sodium.to_base64(signing_key.privateKey, _sodium.base64_variants.ORIGINAL);
+
+
+    // Generate encryption key pair
+    const encr_key = sodium.crypto_box_keypair();
+    const encr_key_public_base64 = sodium.to_base64(encr_key.publicKey, _sodium.base64_variants.ORIGINAL);
+    const encr_key_private_base64 = sodium.to_base64(encr_key.privateKey, _sodium.base64_variants.ORIGINAL);
+
+    // Create a file
+    let content = "ALL KEYS WILL BE BASE64 ENCODED";
+    content = "\n------- Key pair for signing -------\n";
+    content += "Public key: " + signing_key_public_base64 + "\n";
+    content += "Private key: " + signing_key_private_base64 + "\n";
+    content += "\n------- Key pair for encryption -------\n";
+    content += "Public key: " + encr_key_public_base64 + "\n";
+    content += "Private key: " + encr_key_private_base64 + "\n";
+    
+    fs.writeFileSync('keypair.txt', content);
+    console.log("File created. Check keypair.txt file for details");
+
+})()
